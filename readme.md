302faba56e51c4bb548f77c4c1db0414984b0f45
Michał Szeniak <michal.szeniak@gmail.com>
2020-07-12 20:33:15 +0200

    feat: simple counter

--- readme.md
+++ readme.md
@@ -1,13 +1,4 @@
-![cover](https://cotenfrontend.pl/img/cover.png)
-
-# WTF Webpack Starter Kit 
-
-Simple toolkit for your static websites development.
-
-## How to use it?
-
-Click on `Use this template` button to generate a new repository based on this starter kit. After that clone it to your computer, go to the project directory in console and type `npm install`.
-
+![cover]()
 ## Available scripts
 
 `npm run start` - runs development mode
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,29 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+const counter = document.querySelector('.counter--js');
+const addButton = document.querySelector('.add-glass--js');
+const removeButton = document.querySelector('.remove-glass--js');
+
+let counterValue = 0;
+
+addButton.addEventListener('click', () => {
+  counterValue++;
+  console.log(counterValue);
+  counter.innerHTML = counterValue;
+});
+
+removeButton.addEventListener('click', () => {
+  if (counterValue > 0) {
+    counterValue--;
+  }
+  console.log(counterValue);
+  counter.innerHTML = counterValue;
+});
