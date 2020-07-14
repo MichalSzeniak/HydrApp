d11f388192152f8e3e932d5955abd8020cea06c7
Michał Szeniak <michal.szeniak@gmail.com>
2020-07-14 22:06:28 +0200

    feat: local storage

--- src/js/index.js
+++ src/js/index.js
@@ -11,19 +11,32 @@ console.log('HELLO 🚀');
 const counter = document.querySelector('.counter--js');
 const addButton = document.querySelector('.add-glass--js');
 const removeButton = document.querySelector('.remove-glass--js');
+const key = new Date().toLocaleString().slice(0, 10)
+
 
 let counterValue = 0;
 
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+  counterValue = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+
+counter.innerHTML = counterValue;
+
+
 addButton.addEventListener('click', () => {
   counterValue++;
-  console.log(counterValue);
   counter.innerHTML = counterValue;
+  localStorage.setItem(key, counterValue);
 });
 
 removeButton.addEventListener('click', () => {
   if (counterValue > 0) {
     counterValue--;
   }
-  console.log(counterValue);
   counter.innerHTML = counterValue;
+  localStorage.setItem(key, counterValue);
 });
