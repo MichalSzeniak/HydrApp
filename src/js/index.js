2abaeb88d5717017f7e5e32c90772caf788a4db6
Michał Szeniak <michal.szeniak@gmail.com>
2020-07-16 21:35:06 +0200

    feat: simple navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from './pwa.js';
 registerSW();
 
 /* place your code below */
@@ -11,8 +11,7 @@ console.log('HELLO 🚀');
 const counter = document.querySelector('.counter--js');
 const addButton = document.querySelector('.add-glass--js');
 const removeButton = document.querySelector('.remove-glass--js');
-const key = new Date().toLocaleString().slice(0, 10)
-
+const key = new Date().toLocaleString().slice(0, 10);
 
 let counterValue = 0;
 
@@ -25,7 +24,6 @@ if (localStorageValue) {
 }
 
 counter.innerHTML = counterValue;
-
 
 addButton.addEventListener('click', () => {
   counterValue++;
@@ -40,3 +38,38 @@ removeButton.addEventListener('click', () => {
   counter.innerHTML = counterValue;
   localStorage.setItem(key, counterValue);
 });
+
+const maslo = localStorage.getItem(key);
+console.log(maslo);
+
+// History of glasses
+
+const values = [];
+const keys = Object.keys(localStorage);
+let i = keys.length;
+while (i--) {
+  values.push({
+    data: keys[i],
+    value: localStorage.getItem(keys[i]),
+  });
+  console.log(values);
+}
+
+
+
+const checkbox = document.querySelector(".checkbox");
+const check = document.querySelector(".check");
+
+
+checkbox.addEventListener('click', ()=> {
+  if (checkbox.checked == true) {
+    console.log("prawda");
+    check.classList.add("checked");
+    document.documentElement.style.setProperty('--text-color','white');
+  }
+
+  else {
+    check.classList.remove("checked");
+    document.documentElement.style.setProperty('--text-color','rgb(24, 24, 24)');
+  }
+})
